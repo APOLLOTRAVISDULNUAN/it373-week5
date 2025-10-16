c68e4944694e744da3ae4994b431711d1ca3dc84
apollotravisdulnuan <apollotravis.dulnuan@evsu.edu.ph>
2025-10-16 22:51:32 +0800

    added comments view and csrf and some ready made css

--- pages/models.py
+++ pages/models.py
@@ -16,14 +16,14 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
-    author = models.CharField(max_length=100)
-    text = models.TextField()
+class Comment(models.Model):        #neww
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    name = models.CharField(max_length=80)
+    body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Comment by {self.author} on {self.post_id}."
+        return f'Comment by {self.name}'
 
 class Student(models.Model):
     first_name = models.CharField(max_length=100)
